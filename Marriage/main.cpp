905c86c5e5348a8bd5009ad6d2e52a9625b6fb3c
JosephTLyons <josephlyons@Josephs-MacBook-Air.local>
2016-12-24 05:58:46 -0500

    Initial Commit

---
+++ Marriage/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Marriage
+//
+//  Created by Joseph Lyons on 12/24/16.
+//  Copyright © 2016 Joseph Lyons. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
